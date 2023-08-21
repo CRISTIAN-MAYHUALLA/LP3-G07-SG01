1399f9557446f86ce80f882ae2127f98d0f9d869
CRISTIAN MAYHUALLA <cristian.mayhualla@ucsm.edu.pe>
2023-08-20 23:55:44 -0500

    CAMBIO07

---
+++ regalo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class regalo {
+
+    public static void main(String[] args) {
+        int vida, rp;
+        boolean fl = false;
+        vida = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            vida++;
+            System.out.println("Vida:" + vida + "\t¿Cuál es el número secreto?");
+            rp = sc.nextInt();
+            if (rp == 4) {
+                fl = true;
+            }
+        } while (vida < 3 && !fl);
+
+        if (fl) {
+            if (vida == 1) {
+                System.out.println("¡Adivinaste con 1 vida! Tu premio es un chocolate.");
+            } else if (vida == 2) {
+                System.out.println("¡Adivinaste con 2 vidas! Tu premio es una zanahoria.");
+            } else {
+                System.out.println("¡Adivinaste! Tu premio es un carbon.");
+            }
+        } else {
+            System.out.println("Perdiste. Tu premio es un caramelo.");
+        }
+
+        System.out.println("Usaste " + vida + " vidas.");
+    }
+}
